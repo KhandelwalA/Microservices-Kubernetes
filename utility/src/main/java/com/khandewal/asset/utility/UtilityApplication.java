fbb990b1b260a736ef389c5ac2e31fd9bc5188fe
abhishekkhandelwal01 <abhishek.khandelwal01@nagarro.com>
2020-06-08 23:07:04 +0530

    making project minikube compatible and adding spring cloud gateway in the architecture

--- utility/src/main/java/com/khandewal/asset/utility/UtilityApplication.java
+++ utility/src/main/java/com/khandewal/asset/utility/UtilityApplication.java
@@ -2,6 +2,7 @@ package com.khandewal.asset.utility;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class UtilityApplication {
